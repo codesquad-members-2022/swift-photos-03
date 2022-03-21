13ba4fb526f8831b37519c9adf1c6a7b87c84880
kimdongjune <jun36817@naver.com>
2022-03-21 12:44:02 +0900

    커밋메시지 추가

--- PhotoAlbum/PhotoAlbum/ViewController.swift
+++ PhotoAlbum/PhotoAlbum/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("프로젝트 파일 변경하였습니다!")
     }
 
 
