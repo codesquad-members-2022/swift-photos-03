d1928c659879f5b5cf8b941dfa263daefcf71849
Jihee hwang <hwangjihee94@gmail.com>
2022-03-21 12:20:53 +0900

    [ Test ]

--- PhotoAlbum/PhotoAlbum/ViewController.swift
+++ PhotoAlbum/PhotoAlbum/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("dd")
         // Do any additional setup after loading the view.
     }
 
